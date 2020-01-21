920342016f96f78d881338b508b73d43a94fb0c4
Shayne Clausson <shayne.clausson@extendaretail.com>
2020-01-21 17:51:50 +0100

    Add missing provider metadata

--- providers.tf
+++ providers.tf
@@ -1,3 +1,13 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "gcs" {}
+  required_version = "= 0.12.18"
+  required_providers {
+    google = "~> 2.7"
+    gsuite = "~> 0.1.35"
+  }
+}
+
 provider "google" {
   version = "~> 2.7"
   region  = "europe-west-1"
